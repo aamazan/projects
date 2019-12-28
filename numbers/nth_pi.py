829927c18feb5cada1892a21a263bad945932ef6
Armaan Amazan <armaan.amazan@gmail.com>
2019-12-28 16:46:22 -0700

    made it so requesting a single number of pi doesn't return a decimal

--- numbers/nth_pi.py
+++ numbers/nth_pi.py
@@ -6,8 +6,12 @@ n = int(input())
 
 def digits(num):
     p = str(math.pi)
-    num = p[:(num + 1)]
-    return num
+    if num == 1:
+        return num
+    else:
+        num = p[:(num + 1)]
+        return num
 
 print(digits(n))
- 
+
+# TODO: implement rounding
